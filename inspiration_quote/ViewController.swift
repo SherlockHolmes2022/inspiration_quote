0b60acf7754a160cf00689fa94d2f9a03801f067
SherlockHolmes2022 <sherlockholmessh8@gmail.com>
2023-05-01 03:30:24 -0700

    add change image action

--- inspiration_quote/ViewController.swift
+++ inspiration_quote/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var first_quote: UIImageView!
+    var count = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        first_quote.image = UIImage(imageLiteralResourceName: "index")
+        
     }
 
-
+    @IBAction func chnage_image(_ sender: UIButton) {
+        first_quote.image = UIImage(imageLiteralResourceName: "100-of-the-Most-Uplifting-Quotes-Ever5-scaled")
+        print("Count is " , count + 5)
+    }
+    
 }
 
